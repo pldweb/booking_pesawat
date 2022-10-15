211d50eb1c0ad55c7821bc28908134160e7367ff
paldi <paldi@gmail.com>
2022-10-15 15:16:20 +0700

    Tambah Auth State & User Model

---
+++ lib/cubit/auth_cubit.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: depend_on_referenced_packages
+
+import 'package:bloc/bloc.dart';
+import 'package:booking_pesawat/models/user_model.dart';
+import 'package:equatable/equatable.dart';
+
+part 'auth_state.dart';
+
+class AuthCubit extends Cubit<AuthState> {
+  AuthCubit() : super(AuthInitial());
+}
